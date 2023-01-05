8524ff3f4c6f1d1a0615c2f988891ceb1c874c67
Rafa535 <115497147+Rafa535@users.noreply.github.com>
2023-01-04 23:13:34 -0300

    Add files via upload

---
+++ projeto C-11.js
@@ -0,0 +1,31 @@
+
+var sea, seaImg;
+var ship, shipImg;
+
+function preload(){
+  seaImg = loadImage("sea.png");
+  shipImg = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
+}
+
+function setup(){
+  createCanvas(400,400);
+  sea = createSprite (300, 180,  600, 20);
+  ship = createSprite (100, 300, 10, 50);
+
+  ship.addAnimation("Img", shipImg);
+  ship.scale=0.2;
+  ship.x=50;
+}
+
+function draw() {
+  background("blue");
+  drawSprites();
+
+  sea.addImage("sea", seaImg)
+  sea.velocityX=-2
+  console.log(sea.x);
+
+  if(sea.x < 0){
+    sea.x = sea.width/2;
+  }
+}
